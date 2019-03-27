79bf062dee657c6567450268cd35ed2026f991a6
andresmontoyab <and.montoya18@gmail.com>
2019-03-27 17:44:42 -0500

    Exam chapter 2 Results

---
+++ OCP/src/main/java/OCP/chapter2/examOCPChapter2.java
@@ -0,0 +1,30 @@
+package OCP.chapter2;
+
+public class examOCPChapter2 {
+
+    public static void main(String[] args) {
+        /**
+         * 1. Good
+         * 2. Good
+         * 3. Good -> Abstract class are not valid functional interfaces, even if only have a one abstract method.
+         * 4. Bad -> for lambdas (() ->) is not valid.
+         * 5. Good
+         * 6. Good
+         * 7. Good
+         * 8. Good
+         * 9. Bad
+         * 10. Good
+         * 11. Good
+         * 12. Bad
+         * 13. Good
+         * 14. Good
+         * 15. Good
+         * 16. Good
+         * 17. Good
+         * 18. Bad -> Object composition has no notion of inheritance so you have to marked public if you want to use it in other classes in differente package.
+         * 19. Bad
+         * 20. Good
+         * Average -> 15/20 -> 75%
+         */
+    }
+}
